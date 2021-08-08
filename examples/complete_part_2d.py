c101bbeacc0d871ce1c7c959ddf9a61e2429da0b
Mohammadreza Khoshbin <m.khoshbin@live.com>
2021-08-08 12:55:27 +0430

    imported the voxelpart module in __init__.py and updated the example scripts.

--- examples/complete_part_2d.py
+++ examples/complete_part_2d.py
@@ -1,7 +1,7 @@
 """In this example, a complete part with a shape of 50*50 voxels is created.
 The part is then output to abaqus with a uniform scale of 0.02 in all directions."""
 
-import vcams.voxelpart
+import vcams
 
 results_path = 'path\\to\\results\\directory'
 
