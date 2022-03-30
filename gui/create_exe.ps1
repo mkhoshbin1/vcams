f8b0cc6af72a9dd20d95a622d4cae94b0dcaeb2f
Mohammadreza Khoshbin <m.khoshbin@live.com>
2022-03-30 14:35:54 +0430

    - exe seems to have stabilized.

--- gui/create_exe.ps1
+++ gui/create_exe.ps1
@@ -4,17 +4,26 @@
 #   1. UPX must be installed.
 #      It's available in chocolatey or at https://github.com/upx/upx/tree/v3.96.
 
+# Use --console and run the executable in cmd when debugging.
+
 ..\venv\Scripts\Activate.ps1
 
-pyinstaller --onefile --clean --console `
+pyinstaller --onefile --clean --noupx `
     --add-data 'main_window.ui;.' `
+    --add-data 'resources\main_gui_resources.qrc;.\resources\' `
     --icon=".\resources\icon.ico" `
     --splash=".\resources\splash.png" `
     --paths .. `
-    --collect-submodules numpy `
     --name VCAMS `
     main_gui.py
-#    VCAMS.spec
-# --noupx
 
+#    VCAMS.spec
+# 
+#    --hidden-import scipy `
+#    --hidden-import skimage `
+#    --hidden-import uarray `
+#    --collect-submodules numpy `
+#    --collect-submodules scipy `
+#    --collect-submodules skimage `
+#    --collect-submodules uarray `
 pause
